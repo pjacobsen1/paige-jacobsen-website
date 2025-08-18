34f598a064a7dd276a9cd656b7141b752c369b3f
Paige Jacobsen <pjacobsen@uchicago.edu>
2025-08-18 14:11:00 -0500

    Base Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,4 @@
 import './global.css'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
-
-const cx = (...classes) => classes.filter(Boolean).join(' ')
 
 export default function RootLayout({
   children,
@@ -12,22 +6,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html
-      lang="en"
-      className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
-      )}
-    >
+    <html>
       <head>
         <title>Paige Jacobsen</title>
       </head>
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body>
+        <main>
           {children}
-          <Analytics />
-          <SpeedInsights />
         </main>
       </body>
     </html>
---
+++ app/projects/page.tsx
@@ -0,0 +1,36 @@
+import { Truculenta } from 'next/font/google'
+
+const truculenta = Truculenta({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+export default function Projects() {
+  return (
+    <div>
+
+        <nav className="absolute top-0 right-0 pr-8 pt-4">
+            <ul className="flex flex-row gap-8">
+                <li>
+                    <a href="/" className={truculenta.className + " text-xl text-[#68875B] font-bold hover:underline"}>Home</a>
+                </li>
+                <li>
+                    <a href="/projects" className={truculenta.className + " text-xl text-[#68875B] font-bold hover:underline"}>Projects</a>
+                </li>
+                <li>
+                    <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className={truculenta.className + " text-xl text-[#68875B] font-bold hover:underline"}>Resume</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <main className="bg-[#fff0db] min-h-screen flex flex-col items-start justify-center p-12">
+        
+            <p>
+                projects
+            </p>
+
+        </main>
+
+    </div>
+  );
+}
